7b030f1de4548a1308922df463d5bb42600188c1
Sudaisib <oladosuibrahim1999@gmail.com>
2026-08-21 17:03:32 +0100

    Add application startup logging

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -3,3 +3,5 @@ console.log("CommerceFlow application started");
 const applicationName = "CommerceFlow";
 
 console.log(`Welcome to ${applicationName}`);
+console.log("Homepage initialized successfully");
+
